c33973ca52770f357407c594592a31b04ff8cbce
Allen Blount <40569596+ajb88@users.noreply.github.com>
2019-05-09 09:13:23 -0400

    Initial commit

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,50 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+	@color
+	M=0		//Default color is white
+
+(LOOP)
+
+	@SCREEN		//Top left pixel address
+	D=A 		
+	@pixels
+	M=D 		//Pixel address 16384
+
+	@KBD		//Keyboard address
+	D=M
+
+	@BLACK
+	D;JGT		// If M[keyboard]>0 goto BLACK
+
+	@color
+	M=0			//Set color to white
+
+	@COLOR_SCREEN
+	0;JMP		//Jump to subroutine that colors screen white
+
+(BLACK)
+	@color
+	M=-1			//Change color to black
+
+(COLOR_SCREEN)
+	@color
+	D=M
+
+	@pixels
+	A=M			//Indirect address
+	M=D 		//Color Pixels
+
+	@pixels
+	M=M+1
+	D=M
+
+	@24576
+	D=D-A
+
+	@COLOR_SCREEN
+	D;JLT
+
+	@LOOP
+	0;JMP
